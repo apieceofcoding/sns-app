55b4d4a658a39d1a960a6892fe0bb634bd9e4168
sanserif28 <sanserif28@gmail.com>
2026-09-20 23:31:42 +0900

    refactor: remove redundant status from trace response

--- src/main/java/com/apiece/springboot_sns_sample/api/demo/TraceResponse.java
+++ src/main/java/com/apiece/springboot_sns_sample/api/demo/TraceResponse.java
@@ -3,7 +3,6 @@ package com.apiece.springboot_sns_sample.api.demo;
 import java.util.List;
 
 public record TraceResponse(
-        String status,
         String message,
         List<Long> rankedPostIds
 ) {
